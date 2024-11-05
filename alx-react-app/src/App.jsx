f72220071bacd3b691fe392520abf99de6e0daa5
Akosuamercy <akosuansaa22@gmail.com>
2024-11-06 06:16:57 +1300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -23,7 +23,9 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+           <WelcomeMessage /> {/* This is the WelcomeMessage component */}
         </p>
+        
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
