dc13a2533c72419c07e8c7c57976feb280d5bf9f
Mercy Asare <akosuansaa22@gmail.com>
2024-11-19 04:21:11 +1300

    added new changes

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
     return (
-        <main>
+        <main style={{padding: '30px', fontSize:'20' , color:"grey"}}>
           <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
