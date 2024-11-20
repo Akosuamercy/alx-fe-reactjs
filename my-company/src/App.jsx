b77d77326ec0dc6baf4be62d70b6589186aa8c8c
Mercy Asare <akosuansaa22@gmail.com>
2024-11-21 03:00:22 +1300

    add changes

---
+++ my-company/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from 'C:/Users/Mercy Asare/Desktop/alx-fe-reactjs/my-company/src/components/Home';
+import About from 'C:/Users/Mercy Asare/Desktop/alx-fe-reactjs/my-company/src/components/About';
+import Services from 'C:/Users/Mercy Asare/Desktop/alx-fe-reactjs/my-company/src/components/Services';
+import Contact from 'C:/Users/Mercy Asare/Desktop/alx-fe-reactjs/my-company/src/components/Contact';
+import Navbar from 'C:/Users/Mercy Asare/Desktop/alx-fe-reactjs/my-company/src/components/Navbar';
+
+function App() {
+  return (
+    <></>
+  )
+}
+
+export default App;
