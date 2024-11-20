91565ba70fba14fe25b1dd595bcce0b2482c25bb
Mercy Asare <akosuansaa22@gmail.com>
2024-11-21 03:51:08 +1300

    update

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from 'C:/Users/Mercy Asare/Desktop/alx-fe-reactjs/my-company/src/components/Home';
+import Home from './components/Home';
 import About from 'C:/Users/Mercy Asare/Desktop/alx-fe-reactjs/my-company/src/components/About';
 import Services from 'C:/Users/Mercy Asare/Desktop/alx-fe-reactjs/my-company/src/components/Services';
 import Contact from 'C:/Users/Mercy Asare/Desktop/alx-fe-reactjs/my-company/src/components/Contact';
@@ -8,8 +8,23 @@ import Navbar from 'C:/Users/Mercy Asare/Desktop/alx-fe-reactjs/my-company/src/c
 
 function App() {
   return (
-    <></>
-  )
+    <div className="App">
+      <Router>
+      <Navbar /> 
+        <Routes>
+         <Route>
+           <Route index element={<Home />} />
+           <Route path="Navbar" element={<Navbar />} />
+           <Route path="About" element={<About />} />
+           <Route path="Services" element={<Services />} />
+           <Route path="Contact" element={<Contact />} />
+         </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
+
+
 
 export default App;
