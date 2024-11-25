5b8bd7115dfe5db9f16a689fe63d68a7bc8f8876
Mercy Asare <akosuansaa22@gmail.com>
2024-11-26 10:51:12 +1300

    add all changes

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+// @ts-ignore
+import RecipeLst from 'C:/Users/Mercy Asare/Desktop/alx-fe-reactjs/recipe-sharing-app/src/components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+
+function App() {
+ 
+  return (
+    <>
+      <h1>Welcome</h1>
+    </>
+  )
+}
+
+export default App
