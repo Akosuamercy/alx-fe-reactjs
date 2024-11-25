4d4ebf9e2edc20926422760d3e0402ad937285bc
Mercy Asare <akosuansaa22@gmail.com>
2024-11-26 11:42:33 +1300

    changes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 // @ts-ignore
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
+import RecipeDetails from './components/RecipeDetails'
+import EditRecipeForm from './components/EditRecipeForm'
 
-function App() {
- 
-  return (
-    <>
-      <h1>Welcome</h1>
-    </>
-  )
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App
+
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
+
 
 export default App
