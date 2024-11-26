303626b40dc5332d3d5bfa06fcb079f445c4ddec
Mercy Asare <akosuansaa22@gmail.com>
2024-11-27 02:43:22 +1300

    added routes to file

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,22 +1,22 @@
 /* eslint-disable no-unused-vars */
 import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeDetails from './components/RecipeDetails'
 import EditRecipeForm from './components/EditRecipeForm'
+import { Routes  } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: App
-
-  },
-]);
-
-const App = () => {
-  return <RouterProvider router={router} />;
-};
-
+    function App() {
+      return (
+        
+         <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="recipes/id" element={<RecipeDetails />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/edit" element={<EditRecipeForm />} />
+          </Routes>
+      )
+    }
 
 export default App
