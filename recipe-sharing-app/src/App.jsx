7607d3201b24f2f45ae719b862b7553294ddfaa0
Mercy Asare <akosuansaa22@gmail.com>
2024-11-27 03:18:17 +1300

    new changes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -7,6 +7,7 @@ import RecipeDetails from './components/RecipeDetails'
 import EditRecipeForm from './components/EditRecipeForm'
 import { Routes  } from 'react-router-dom';
 import FavoritesList from './components/FavoritesList'
+import RecommendationsList from './components/RecommendationsList'
 
     function App() {
       return (
@@ -17,6 +18,7 @@ import FavoritesList from './components/FavoritesList'
           <Route path="/add" element={<AddRecipeForm />} />
           <Route path="/edit" element={<EditRecipeForm />} />
           <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
           </Routes>
       )
     }
