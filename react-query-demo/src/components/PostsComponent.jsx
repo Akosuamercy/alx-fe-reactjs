f12f98510b58f47c6a5676a1f4d42d35e65278ae
Mercy Asare <akosuansaa22@gmail.com>
2024-12-02 11:34:53 +1300

    a react query project

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useQuery } from 'react-query';
+
+const fetchPosts = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts/');
+    return res.json();
+};
+
+const PostsComponent = () => {
+    const { data, isLoading, error, } = useQuery(
+        'posts',
+        fetchPosts,
+
+         {
+            cacheTime: 10 * 60 * 1000,
+            staleTime: 5 * 60 * 1000,
+            refetchOnWindowFocus: true,
+            keepPreviousData: true,
+          }
+
+    );
+  console.log (data);
+
+  
+if (isLoading) return <div>Loading...</div>;
+if (error) return <div>Error loading data</div>;
+
+
+ return(
+    <div>
+        <h1>Posts Components</h1>
+        <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+   );
+}
+
+export default PostsComponent;
