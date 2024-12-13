2a71a4454b39ab80a6ee40aad0817fec172e62af
Mercy Asare <akosuansaa22@gmail.com>
2024-12-14 09:44:00 +1300

    state management for search jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -2,22 +2,49 @@ import React from 'react'
 import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
-const Search ({}) => {
-  
-  
-    return (
-      
-    <div className='search-bar'>
-      <form onSubmit= {handleSubmit}>
-        <input type="text" id="username" placeholder="Github username" required onChange={handleChange} />
-         <button type= "submit">Search</button>
-         <div id="root"></div>
+function Search() {
+    const [formData, setFormData] = useState({
+      username: '',
+      password: ''
+    });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+      };
+
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        alert('Details submitted!');
+      };
+
+      return (
+        <div className='search-bar'>
+        <div style={{ padding: '20px' }}></div>
+          <h2>Github User Search</h2>
+          <form onSubmit= {handleSubmit}>
+            <input 
+              type="text" 
+              id="username" 
+              placeholder="Github Username" required 
+              value {formData.username}
+              onChange={handleChange}
+              style= {{ display: 'block', margin: '15px 0'}}
+            />
+
+            <input 
+              type="password" 
+              name="password" 
+              placeholder="Your Password" required 
+              value {password}
+              onChange={handleChange}
+              style= {{ display: 'block', margin: '15px 0'}}
+            />
+            <button type= "submit">Search</button>    
       </form>  
     </div>
     
   );
 };
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Search />)
+
 
 export default Search;
